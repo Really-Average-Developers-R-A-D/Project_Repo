2d42778464bd0dc3e881d8739a4c5328afad62fa
smatiishin <131989226+smatiishin@users.noreply.github.com>
2024-10-04 09:54:48 -0400

    Updated login.js

--- login.js
+++ login.js
@@ -17,6 +17,7 @@ function loginValidation() {
 function resetPassForm() {
     const emailReset = document.getElementById('pass-reset');
     emailReset.style.display = 'block';
+    document.getElementById('divider').style.display = 'block';
     message.innerHTML = "";
 }
 
@@ -25,12 +26,26 @@ function passReset() {
     const email = document.getElementById('email-reset').value;
     const message = document.getElementById('message');
 
-    // Checks to see if the email field is filled
-    if (email) {
+    // This is a regular expression that validates the email format
+    // No spaces, one or more character before "@", one or more character after "@" before ".", one or more character after "@" 
+    const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Checks to see if the email field is filled amd valid
+    if (emailValid.test(email)) {
         message.innerHTML = 'Further instructions sent to ' + email;
         message.className = 'message success';
     } else {
         message.innerHTML = 'Please enter a valid email address!';
         message.className = 'message error';
     }
+}
+
+// This function improves user interaction by highlighting a role when selected by user
+function selectRole(button) {
+    // Remove the active status from all buttons
+    let buttons = document.querySelectorAll('.role-button');
+    buttons.forEach(btn => btn.classList.remove('active'));
+
+    // Add the active status to user selected button so it can be highlighted
+    button.classList.add('active');
 }
