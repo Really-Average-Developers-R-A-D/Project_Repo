cf929e0e36d166b272cecaec8881ec99a78981f2
Jayden Asbie <100002679+JaydenAsbie@users.noreply.github.com>
2024-11-13 13:15:06 -0500

    Jayden branch2 (#12)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update mysql-service.js
    
    * Update mysql-service.js
    
    * Update api-users.js
    
    * Update mysql-service.js
    
    * Update Webtest.html
    
    * Update api-users.js
    
    * Update landingStudent.html
    
    * Update landingTeacher.html
    
    * Update mysql-service.js
    
    * Update student.css
    
    * Update studentPage.js
    
    * Update teacher.css
    
    * Update teacherPage.js
    
    * update
    
    * update
    
    * Delete mysql-service.js
    
    * Delete api-users.js
    
    * update
    
    * Delete Brockport_logo.jpg
    
    * Delete Webtest.html
    
    * Delete landingStudent.html
    
    * Delete landingTeacher.html
    
    * Delete login.js
    
    * Delete student.css
    
    * Delete studentPage.js
    
    * Delete teacher.css
    
    * Delete teacherPage.js
    
    * Delete weblogin.css
    
    * student page updated
    
    * update comments
    
    * comments finished
    
    * student page updates
    
    * sam changes
    
    * teacher landing page
    
    ---------
    
    Co-authored-by: smatiishin <131989226+smatiishin@users.noreply.github.com>
    Co-authored-by: smati1 <sammatiishin@yahoo.com>

--- api/api-users.js
+++ api/api-users.js
@@ -149,6 +149,42 @@ router.get("/student-courses", async (req, res) => {
     }
 });
 
+
+
+// Route to get the courses for the logged-in teacher
+router.get("/teacher-courses", async (req, res) => {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ error: "Authorization header missing" });
+    }
+
+    // Find the user by username in the database before running a query to get all the user's enrolled classes
+    try {
+        
+        // Decode token
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.decode(token, secret);
+        const username = decoded.username; 
+
+        // Fetch the user details from the database
+        const user = await db.getUserByUsername(username);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        // Query to get course details of the teacher
+        const result = await db.getCoursesByTeaching(username);
+        res.json(result);
+    } catch (error) {
+        console.error("Error fetching teacher courses:", error);
+        res.status(500).json({ error: "Failed to fetch courses" });
+    }
+});
+
+
+
+
 // Route to get available courses for the student
 router.get('/available-courses', async (req, res) => {
     const authHeader = req.headers.authorization;
